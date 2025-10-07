b9685838244d7f6d0c9e430eacd45364e16d9f9a
Pradeep Kumar Maurya <pradeepx135@gmail.com>
2025-10-07 17:21:36 +0530

    Minimum LCM

--- Rating-1000/MimimumLCM.cpp
+++ Rating-1000/MimimumLCM.cpp
@@ -9,10 +9,27 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        // int t= 2;
-        int x=5;
-        cout<<x<<endl;
-
+        int n;
+        int a=0,b=0;
+        cin >>n;
+        if(n%2==0){
+            a=n/2;
+            b=n/2;
+        }
+        else{
+            for(int i=2;i<=sqrt(n);i++){
+                if(n%i==0){
+                    a=max(i,n/i);
+                    b=n-a;
+                    break;
+                }
+            }
+        }
+        if(a==0 || b==0){
+            a=1;
+            b=n-a;
+        }
+        cout<<a<<" "<<b<<"\n";
     }
     return 0;
 }
